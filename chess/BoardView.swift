0827a16bff4e7487f7079e3c2542a9841419c22a
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2022-03-26 13:04:03 +0300

    lkjmhnfgdvd

---
+++ chess/BoardView.swift
@@ -0,0 +1,19 @@
+//
+//  BoardView.swift
+//  chess
+//
+//  Created by Aisha Taleb on 25/03/2022.
+//  Copyright © 2022 GoldThumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+
+}
